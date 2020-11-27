175e51f373356502b9f3874a073855c597c064d9
agentsimon <31639346+agentsimon@users.noreply.github.com>
2020-11-27 10:58:08 +0700

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "simon"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "binh12345"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "xxxx"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "xxxxxx"	// replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 1147121			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "9MM34MIOI5HH6AAA"   // replace XYZ with your channel write API Key
+#define SECRET_CH_ID 0000000			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
